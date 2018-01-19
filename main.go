150c874b0b64ad0f20228f0dddbc178e50f9fe89
Gianni Salinetti <gbsalinetti@extraordy.com>
2018-01-19 14:44:29 +0100

    All encoding and output to separate functions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/fsouza/go-dockerclient"
@@ -24,7 +25,54 @@ func printHelp() {
 	flag.PrintDefaults()
 }
 
-func main() {
+// We print the images cycling the slice containint APIImages structs
+//type APIImages struct {
+//ID          string            `json:"Id" yaml:"Id" toml:"Id"`
+//RepoTags    []string          `json:"RepoTags,omitempty" yaml:"RepoTags,omitempty" toml:"RepoTags,omitempty"`
+//Created     int64             `json:"Created,omitempty" yaml:"Created,omitempty" toml:"Created,omitempty"`
+//Size        int64             `json:"Size,omitempty" yaml:"Size,omitempty" toml:"Size,omitempty"`
+//VirtualSize int64             `json:"VirtualSize,omitempty" yaml:"VirtualSize,omitempty" toml:"VirtualSize,omitempty"`
+//ParentID    string            `json:"ParentId,omitempty" yaml:"ParentId,omitempty" toml:"ParentId,omitempty"`
+//RepoDigests []string          `json:"RepoDigests,omitempty" yaml:"RepoDigests,omitempty" toml:"RepoDigests,omitempty"`
+//Labels      map[string]string `json:"Labels,omitempty" yaml:"Labels,omitempty" toml:"Labels,omitempty"`
+
+func jsonEnc(imgs []docker.APIImages) (string, error) {
+	jsonSer, err := json.Marshal(imgs)
+	if err != nil {
+		return "", err
+	}
+	result := fmt.Sprintf("%s", string(jsonSer))
+	return result, nil
+}
+
+func yamlEnc(imgs []docker.APIImages) (string, error) {
+	yamlser, err := yaml.Marshal(imgs)
+	if err != nil {
+		return "", err
+	}
+	result := fmt.Sprintf("%s", string(yamlser))
+	return result, nil
+}
+
+func plainTextAll(imgs []docker.APIImages) error {
+	if len(imgs) == 0 {
+		return errors.New("Empty image list")
+	}
+	for _, img := range imgs {
+		fmt.Println("ID: ", img.ID)
+		fmt.Println("RepoTags: ", img.RepoTags)
+		fmt.Println("Created: ", img.Created)
+		fmt.Println("Size: ", img.Size)
+		fmt.Println("VirtualSize: ", img.VirtualSize)
+		fmt.Println("ParentID: ", img.ParentID)
+		fmt.Println("RepoDigests: ", img.RepoDigests)
+		fmt.Println("Labels: ", img.Labels)
+		fmt.Printf("\n")
+	}
+	return nil
+}
+
+func checkMultiEncFlag(
 
 	jsonON := flag.Bool("json", false, "Use JSON encoding")
 	yamlON := flag.Bool("yaml", false, "Use YAML encondig")
@@ -52,49 +100,34 @@ func main() {
 		panic(err)
 	}
 
-	// We print the images cycling the slice containint APIImages structs
-	//type APIImages struct {
-	//ID          string            `json:"Id" yaml:"Id" toml:"Id"`
-	//RepoTags    []string          `json:"RepoTags,omitempty" yaml:"RepoTags,omitempty" toml:"RepoTags,omitempty"`
-	//Created     int64             `json:"Created,omitempty" yaml:"Created,omitempty" toml:"Created,omitempty"`
-	//Size        int64             `json:"Size,omitempty" yaml:"Size,omitempty" toml:"Size,omitempty"`
-	//VirtualSize int64             `json:"VirtualSize,omitempty" yaml:"VirtualSize,omitempty" toml:"VirtualSize,omitempty"`
-	//ParentID    string            `json:"ParentId,omitempty" yaml:"ParentId,omitempty" toml:"ParentId,omitempty"`
-	//RepoDigests []string          `json:"RepoDigests,omitempty" yaml:"RepoDigests,omitempty" toml:"RepoDigests,omitempty"`
-	//Labels      map[string]string `json:"Labels,omitempty" yaml:"Labels,omitempty" toml:"Labels,omitempty"`
-
 	if *jsonON && !*yamlON && !*textON {
 		// Print the conent in json format
-		jsonSer, err := json.Marshal(imgs)
+		res, err := jsonEnc(imgs)
 		if err != nil {
-			panic(err)
+			fmt.Printf("Json encoding error: %s\n", err)
+			os.Exit(1)
 		}
-		fmt.Printf("%s", string(jsonSer))
+		fmt.Printf("%s", res)
 		os.Exit(0)
 	}
 
 	if !*jsonON && *yamlON && !*textON {
 		// Print the conent in json format
-		yamlser, err := yaml.Marshal(imgs)
+		res, err := yamlEnc(imgs)
 		if err != nil {
-			panic(err)
+			fmt.Println("Yaml encoding error: %s\n", err)
+			os.Exit(1)
 		}
-		fmt.Printf("%s", string(yamlser))
+		fmt.Printf("%s", res)
 		os.Exit(0)
 	}
 
 	// We want text to be the default output format
 	if (!*jsonON && !*yamlON && *textON) || (!*jsonON && !*yamlON && !*textON) {
-		for _, img := range imgs {
-			fmt.Println("ID: ", img.ID)
-			fmt.Println("RepoTags: ", img.RepoTags)
-			fmt.Println("Created: ", img.Created)
-			fmt.Println("Size: ", img.Size)
-			fmt.Println("VirtualSize: ", img.VirtualSize)
-			fmt.Println("ParentID: ", img.ParentID)
-			fmt.Println("RepoDigests: ", img.RepoDigests)
-			fmt.Println("Labels: ", img.Labels)
-			fmt.Printf("\n")
+		err := plainTextAll(imgs)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
 		}
 		os.Exit(0)
 	}
